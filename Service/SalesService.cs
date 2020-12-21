acea192b55138b18e10d817291f12ff111dfd7ee
kamil <kamil467@gmail.com>
2020-12-21 19:58:27 +0530

    Service added

---
+++ Service/SalesService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using NemisisWebApp.ViewModel;
+
+namespace NemisisWebApp.Service
+{
+    public class SalesService
+    {
+        private readonly NemisisBillingEntities context;
+
+        public SalesService()
+        {
+            this.context = new NemisisBillingEntities();
+        }
+
+        public Task<List<ViewSaleModel>> GetAllInvoices()
+        {
+
+            var taskResult = Task.Factory.StartNew(() =>
+            {
+                var invoicesList = this.context.CustomerInvoices
+                    .Include("Customer")
+                    .Select(i => new ViewSaleModel {
+                    CustomerName =       i.Customer.Name,
+                    InvoiceDate = i.DateOfCreation.Value,
+                    InvoiceNumber = i.InvoiceTIN,
+                    SalesValue = i.TotalAmountPaidByCustomer.Value,
+                      DiscountAmount = i.TotalDiscountInAmount.Value                   
+                    }).ToList();
+
+                return invoicesList;
+            }
+            );
+
+            return taskResult;
+        }
+    }
+}
