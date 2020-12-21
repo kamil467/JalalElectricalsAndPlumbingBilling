1fb7c44d8e524102b710574aa18f53e6e68d0b46
kamil <kamil467@gmail.com>
2020-12-21 19:59:50 +0530

    View Model added

---
+++ ViewModel/ViewSaleModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NemisisWebApp.ViewModel
+{
+    public class ViewSaleModel
+    {
+        public virtual string InvoiceNumber { get; set; }
+
+        public virtual DateTime InvoiceDate { get; set; }
+
+        public virtual string CustomerName { get; set; }
+
+        public virtual decimal SalesValue { get; set; }
+
+        public virtual decimal DiscountAmount { get; set; }
+
+    }
+}
